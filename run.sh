871abe717c9c35bc93c34647705476fc6b26cdbf
miguelgonrod <miguelgonrod2004@gmail.com>
2024-09-18 10:57:15 -0500

    Feat: Added the fixed docker file

--- run.sh
+++ run.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 docker build -f build/uros.Dockerfile -t microrosimage .
-docker run -it \
-	-e DISPLAY=$DISPLAY \
-           -v /tmp/.X11-unix:/tmp/.X11-unix \
-           -v $HOME/.Xauthority:/root/.Xauthority \
-	--network host \
-	--mount src="./app",target="/home/robot/app",type=bind \
-	--device=/dev/ttyUSB0:/dev/ttyUSB0 \
-	microrosimage:latest
+
+if [[ "$1" == "--flash" ]]; then
+
+	docker run -it \
+		-e DISPLAY=$DISPLAY \
+			-v /tmp/.X11-unix:/tmp/.X11-unix \
+			-v $HOME/.Xauthority:/root/.Xauthority \
+		--network host \
+		--mount src="./app",target="/home/robot/app",type=bind \
+		--device=/dev/ttyUSB0:/dev/ttyUSB0 \
+		microrosimage:latest
+
+else
+	docker run -it \
+		-e DISPLAY=$DISPLAY \
+			-v /tmp/.X11-unix:/tmp/.X11-unix \
+			-v $HOME/.Xauthority:/root/.Xauthority \
+		--network host \
+		--mount src="./app",target="/home/robot/app",type=bind \
+		microrosimage:latest
